c813c01f212037834812e00d2cb15f5a9fa673b7
TA Dang Chau Khue <khue118@gmail.com>
2025-07-04 17:37:26 +0200

    vid 9.3.1

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,30 @@
+import './Login.scss'
 const Login=(props)=>{
     return(
-        <div className="login-container">
-            login
-            <button>test bootstrap</button>
+        <div className="login-container mt-3">
+            <div className="container">
+                <div className="row">
+                    <div className="content-left col-7 ">
+                        <div className='brand'>
+                            VCSI Learning
+                        </div>
+                        <div className='detail'>
+                            VCSI Learning helps you master worksite applications
+                        </div>
+                    </div>
+                    <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+                        <input type="text" className='form-control' placeholder='Email address or phone number'/>
+                        <input type="password" className='form-control' placeholder='Password'/>
+                        <button className='btn btn-primary'>Login</button>
+                        <span className='text-center'>Forgot your pasword?</span>
+                        <hr/>
+                        <div className='text-center'>
+                            <button className='btn btn-success'>Create new account</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
 
         </div>
     )
