f88dd915ed71e499eacf3cb4ff1aea8a4bffcfec
TA Dang Chau Khue <khue118@gmail.com>
2025-07-16 21:22:56 +0200

    vid 10.4

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 
 import './App.scss';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-
-} from "react-router-dom";
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
+import {BrowserRouter as Router} from "react-router-dom";
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Users from './components/ManageUsers/Users';
 import Nav from './components/Navigation/Nav';
 import{useEffect,useState} from 'react';
 import _ from "lodash";
-
-
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
   const [account,setAccount] = useState({});
@@ -27,36 +18,12 @@ function App() {
   },[]);
   return (
   <Router>
-    <div className ='app-container'> 
-    {account && !_.isEmpty(account) && account.isAuthenticated && <Nav />
-    }
+    <div className='app-header'>
+          <Nav />
+    </div>
 
-      <Switch>
-          <Route path="/news">
-            news
-          </Route>
-          <Route path="/about">
-            about
-          </Route>
-          <Route path="/contact">
-            contact
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/" exact>
-            home
-          </Route>
-          <Route path="*">
-            404 not found
-          </Route>
-        </Switch>
+    <div className ='app-container'> 
+        <AppRoutes />
     </div>
         <ToastContainer
         position="bottom-center"
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import './Login.scss';
 import{ useHistory } from "react-router-dom";
 import { toast } from 'react-toastify';
@@ -57,7 +57,15 @@ const Login=(props)=>{
         }
             
     }
+    
+    useEffect(()=>{
+            let session = sessionStorage.getItem('account');
+            if(session){
+            history.push("/");
+            window.location.reload();
+            }
 
+    }, [])
     return(
         <div className="login-container">
             <div className="container">
